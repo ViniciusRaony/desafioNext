e1502aecd72f04b37da8241b7492bd741b7522b1
ViniciusRaony <viniciusraony@hotmail.com>
2022-06-10 08:47:16 -0300

    Atualizado response

--- app.py
+++ app.py
@@ -13,98 +13,82 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(40), nullable=False)
-    activated = db.Column(db.Boolean, default=False)
-    hash = db.Column(db.String(100))
 
     def to_json(self):
-        return {"id": self.id, "name": self.name, "email": self.email, "password": self.password,
-                "activated": self.activated, "hash": self.hash}
+        return {"id": self.id, "name": self.name, "email": self.email}
 
 
 # READ
-@app.route("/users", methods=["GET"])
+@app.route("/users", methods=["GET"])  # SELECT ALL
 def select_users():
-    users_objects = User.query.all()
-    users_json = [user.to_json() for user in users_objects]  # Criando vetor para armazenar json
-    return generate_response(200, "users", users_json, {})
+    dic_output_msg = {"user": None,
+                      "200": "Fim listagem"}
+    users_select = User.query.all()
+    dic_output_msg["user"] = [user.to_json() for user in users_select]
+    return Response(json.dumps(dic_output_msg))
 
 
-@app.route("/user/<id>", methods=["GET"])
+@app.route("/user/<id>", methods=["GET"])  # SELECT POR ID
 def select_user(id):
-    user_object = User.query.filter_by(id=id).first()  # first() retorna o primeiro registro encontrado
-    user_json = user_object.to_json()  # Sem for, busca por id individual
-    return generate_response(200, "user", user_json)
+    dic_output_msg = {"user": None,
+                      "200": "Fim listagem"}
+    user_select = User.query.filter_by(id=id).first()
+    dic_output_msg["user"] = user_select.to_json()
+    return Response(json.dumps(dic_output_msg))
 
 
 # CREATE
 @app.route("/user", methods=["POST"])
 def create_user():
-    body = request.get_json()
+    dic_output_msg = {"user": None,
+                      "201": "Cadastro criado com sucesso!"}
     try:
-        user = User(name=body["name"], email=body["email"], password=body["password"], activated=body["activated"], hash=body["hash"])
+        data = request.get_json()
+        user = User(name=data["name"], email=data["email"])
         db.session.add(user)
         db.session.commit()
-        return generate_response(201, "user", user.to_json(), "Cadastro criado com sucesso!")
-    except Exception as e:
-        print(e)
-        return generate_response(400, "user", {}, "Erro ao criar cadastro")
+        dic_output_msg["user"] = user.to_json()
+        return Response(json.dumps(dic_output_msg))
+    except:
+        return Response(json.dumps("400 Erro ao criar cadastro"))
 
 
 # UPDATE
 @app.route("/user/<id>", methods=["PUT"])
 def update_user(id):
-    user_object = User.query.filter_by(id=id).first()
-    body = request.get_json()
+    dic_output_msg = {"user": None,
+                      "200": "Cadastro atualizado com sucesso!"}
+    user_update = User.query.filter_by(id=id).first()
+    data = request.get_json()
     try:
-        if 'name' in body:
-            user_object.nome = body['name']
-        if 'email' in body:
-            user_object.email = body['email']
-        if 'password' in body:
-            user_object.email = body['password']
-        if 'activated' in body:
-            user_object.email = body['activated']
-        if 'hash' in body:
-            user_object.email = body['hash']
-        db.session.add(user_object)
+        if 'name' in data:
+            user_update.name = data['name']
+        if 'email' in data:
+            user_update.email = data['email']
+        db.session.add(user_update)
         db.session.commit()
-        return generate_response(201, "user", user_object.to_json(), "Cadastro atualizado com sucesso!")
-    except Exception as e:
-        print("Erro", e)
-        return generate_response(400, "user", {}, "Erro ao atualizar cadastro")
+        dic_output_msg["user"] = user_update.to_json()
+        return Response(json.dumps(dic_output_msg))
+    except:
+        return Response(json.dumps("400 Erro ao atualizar cadastro"))
 
 
 # DELETE
 @app.route("/user/<id>", methods=["DELETE"])
 def delete_user(id):
-    user_object = User.query.filter_by(id=id).first()
+    dic_output_msg = {"user": None,
+            "200": "Cadastro deletado com sucesso"}
     try:
-        db.session.delete(user_object)
+        user_delete = User.query.filter_by(id=id).first()
+        dic_output_msg["user"] = user_delete.to_json()
+        db.session.delete(user_delete)
         db.session.commit()
-        return generate_response(200, "user", user_object.to_json(), "Cadastro deletado com sucesso!")
-    except Exception as e:
-        print('Erro', e)
-        return generate_response(400, "user", {}, "Erro ao deletar cadastro")
+        return Response(json.dumps(dic_output_msg))
+    except:
+        return Response(json.dumps("400 Erro ao deletar cadastro"))
 
 
-# GENERATE RESPONSE, OBS: Mensagem opcional
-def generate_response(status, name_of_content, content, mensage=False):
-    body = {}  # criando dicionário p/ retorno.
-    body[name_of_content] = content
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
 
-    if mensage:  # False ou zero
-        body["message"] = mensage
-    return Response(json.dumps(body), status=status, mimetype="application/json")
-
-
-app.run()
-
-'''
-TODO
-FLASK-EMAIL
-MENSAGEM HASH PRA VALIDAR CRIAR ROTA GET P/ HASH: O status do usuario vai ficar false até ele clicar e validar)
-ALTERAR STATUS NO BANCO DE DADOS: Depois que confirmar o email é que o status fica ativo, e ai ele pode fazer login
-MENSAGEM DE NOTIFICAÇÃO: Não precisa mais validar no banco de dados, pois eu vou dar um usuarios.email && usuarios.status=TRUE
-TEMPLATE HTML LOGIN
-'''
